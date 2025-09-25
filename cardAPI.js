a95dfb5baaf7127395a9961b400a356f0a2c5aa2
Nyala Jackson <nymjackson@gmail.com>
2025-09-25 16:52:29 -0400

    structure html work

--- cardAPI.js
+++ cardAPI.js
@@ -1,29 +1,35 @@
-const BASE_API_URL = "https://deckofcardsapi.com/api/deck"
+const BASE_API_URL = "https://deckofcardsapi.com/api/deck";
 let deckId;
 
-export async function getNewDeck(){
-    try{
-    const promiseDeck = await fetch(BASE_API_URL+"/new/shuffle/?deck_count=1")
-    const deckInfo = await promiseDeck.json()
-    deckId = deckInfo.deck_id
-    return deckInfo || false
-    }
-    catch(e){
-        console.log("Couldn't get a new deck.")
-        console.log(e)
-    }
+export async function getNewDeck() {
+  try {
+    const promiseDeck = await fetch(
+      BASE_API_URL + "/new/shuffle/?deck_count=1"
+    );
+    const deckInfo = await promiseDeck.json();
+    deckId = deckInfo.deck_id;
+    return deckInfo || false;
+  } catch (e) {
+    console.log("Couldn't get a new deck.");
+    console.log(e);
+  }
 }
 
-getNewDeck()
+//getNewDeck()
 
-export async function drawCard(count){
-try{
-const promiseCard = await fetch(BASE_API_URL+ deckId+"/draw/?count="+count)
-const hand = await promiseCard.json()
-console.log(hand)
+export async function drawCard(count) {
+  try {
+    const promiseCard = await fetch(
+      BASE_API_URL + deckId + "/draw/?count=" + count
+    );
+    const hand = await promiseCard.json();
+    console.log(hand);
+  } catch (e) {
+    console.log("Unable to draw a card.");
+    console.log(e);
+  }
 }
-catch(e){
-    console.log("Unable to draw a card.")
-    console.log(e)
-}
+
+export async function getDeck(){
+    
 }
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // All logic for index elements 
 import * as deckAPI from "./cardAPI.js"
+const availGames = ["BlackJack", "Cabo"]
 
-const availGames = ["BlackJack"]
