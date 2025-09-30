87520c37d998e246f9492826bf59df178cf00a74
Nyala Jackson <nymjackson@gmail.com>
2025-09-29 21:15:59 -0400

    showGames logic

--- gameRecommendation/scripts/gameAPI.js
+++ gameRecommendation/scripts/gameAPI.js
@@ -1,28 +1,40 @@
 // cac9746b0af44734a45fc0117060dc26
 export const API_KEY = "cac9746b0af44734a45fc0117060dc26"
 export const BASE_API_URL = "https://api.gamebrain.co/v1/games"
-import {searchBar} from './index.js'
-async function initialLoad(){
-    const link = await fetch(BASE_API_URL, {
-        method: "GET",
-        headers:{
-            "x-api-key":API_KEY
-        }
-    });
+import {searchBar, showGames, clearGames, gameLib} from './index.js'
 
-}
+// async function initialLoad(){
+//     const link = await fetch(BASE_API_URL, {
+//         method: "GET",
+//         headers:{
+//             "x-api-key":API_KEY
+//         }
+//     });
+
+// }
 
 export async function searchForGames(e){
+    e.preventDefault()
+    clearGames()
+    gameLib.style.cursor = "progress"
     let searchQuery = "?query="
     console.log(searchBar.value)
     const queryConstruction = searchBar.value.split(" ").join("+")
     searchQuery+= queryConstruction
-    const gameLink = await fetch(BASE_API_URL+searchQuery, {
+    try{
+        const gameLink = await fetch(BASE_API_URL+searchQuery, {
         method: "GET",
         headers:{
             "x-api-key":API_KEY
         }
     });
+
     const jsonLink = await gameLink.json()
     console.log(jsonLink)
+    gameLib.style.cursor = "default"
+    showGames(jsonLink.results)
+    }
+    catch(e){
+        console.log(e)
+    }
 }
--- gameRecommendation/scripts/index.js
+++ gameRecommendation/scripts/index.js
@@ -1,13 +1,40 @@
 import {searchForGames} from "./gameAPI.js"
 const navBar = document.getElementById("nav")
 export const searchBar = document.getElementById("game-search")
+export const gameLib = document.getElementById("game-library")
 const searchBtn = document.getElementById("submit")
 
+
+
 function menuLinkInteraction(e){
-const target = e.target.value
+const target = e.target.textContent
+console.log(target)
 }
-
-
 
 navBar.addEventListener("click", menuLinkInteraction)
 searchBtn.addEventListener("click", searchForGames)
+
+export function showGames(games){
+    const frag = document.createDocumentFragment()
+    for(let i = 0; i < games.length; i ++){
+    const game = games[i]
+    console.log(game)
+    const div = document.createElement("div")
+    const title = document.createElement("h3")
+    const img = document.createElement("img")
+    div.classList.add("game")
+    img.src = game.image
+    title.textContent = game.name
+    div.appendChild(img)
+    div.appendChild(title)
+    frag.appendChild(div)
+    }
+    gameLib.appendChild(frag)
+    
+}
+
+export function clearGames(){
+    while(gameLib.firstChild){
+        gameLib.removeChild(gameLib.firstChild)
+    }
+}
